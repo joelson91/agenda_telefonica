73085271dadbd7142b2dd4236bff589caaed97b2
Joelson <joelsonandradep@gmail.com>
2025-01-20 13:54:31 -0300

    add form validator data required

--- forms/form.py
+++ forms/form.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, TelField, SubmitField
+from wtforms.validators import DataRequired
 
 
 class UserForm(FlaskForm):
-    nome = StringField("Nome")
-    telefone = TelField("Telefone")
+    nome = StringField("Nome", validators=[DataRequired()])
+    telefone = TelField("Telefone", validators=[DataRequired()])
     email = EmailField("Email")
     btn_submit = SubmitField("Salvar")
